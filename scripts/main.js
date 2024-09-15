84aef2d21c95a8d61474f74311099b531c2714e0
Answer <36986768+Answer0303@users.noreply.github.com>
2024-09-15 09:55:24 +0800

    Add files via upload
    添加第一版本的测试网站内容

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!"; //这是一个注释
+
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
